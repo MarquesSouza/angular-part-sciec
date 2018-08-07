5307fcf25ed9695572b029f2736a2c0689bec6c6
Marques <marquesde2013@gmail.com>
2018-08-07 19:56:01 -0300

    Correção de bugs

---
+++ src/app/shared/interface/user.ts
@@ -0,0 +1,11 @@
+export interface user {
+    id: number;
+    name: string;
+    email: string;
+    cpf: string;
+    celular: number;
+    status: boolean;
+    remember_token: string;
+    created_at: string;
+    updated_at: string;
+}
--- src/app/shared/service/auth.service.ts
+++ src/app/shared/service/auth.service.ts
@@ -4,7 +4,7 @@ import {environment} from "../../../environments/environment";
 import { Router } from '@angular/router';
 import {Observable} from "rxjs";
 import  "rxjs/add/operator/do";
-
+import {user} from "../interface/user";
 @Injectable()
 export class AuthService {
 
@@ -18,7 +18,8 @@ export class AuthService {
         return this.http.post<any>(`${environment.api_url}/auth/login`, credentials)
             .do(data => {
                 localStorage.setItem('token', data.token);
-                localStorage.setItem('user', btoa(JSON.stringify(data.user)));
+               // localStorage.setItem('user', btoa(JSON.stringify(data.user)));
+               localStorage.setItem('user', JSON.stringify(data.user));
             });
     }
 
@@ -30,19 +31,19 @@ export class AuthService {
         });
     }
 
-    // getUser(): User {
-    //     return localStorage.getItem('user') ? JSON.parse(atob(localStorage.getItem('user'))) : null;
-    // }
-    //
-    // setUser(): Promise<boolean> {
-    //     return this.http.get<any>(`${environment.api_url}/auth/me`).toPromise()
-    //         .then(data => {
-    //             if (data.user) {
-    //                 localStorage.setItem('user', btoa(JSON.stringify(data.user)));
-    //                 return true;
-    //             }
-    //             return false;
-    //         });
-    // }
+     getUser(): user {
+        return localStorage.getItem('user') ? JSON.parse(atob(localStorage.getItem('user'))) : null;
+     }
+    
+     setUser(): Promise<boolean> {
+         return this.http.get<any>(`${environment.api_url}/auth/me`).toPromise()
+             .then(data => {
+                 if (data.user) {
+                     localStorage.setItem('user', btoa(JSON.stringify(data.user)));
+                     return true;
+                 }
+                 return false;
+             });
+     }
 
 }
