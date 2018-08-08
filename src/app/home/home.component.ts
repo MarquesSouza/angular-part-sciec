33a9c51ab9f2dffe0c835bd3651e06fd0b2edbe3
Marques <marquesde2013@gmail.com>
2018-08-07 21:38:48 -0300

    Correção de bugs

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -26,25 +26,7 @@ export class HomeComponent implements OnInit {
 
     };
 
-    addNewEvent = function(event) {
-        this.eventObj = {
-            'nome': event.nome,
-            'descricao': event.descricao,
-            'local': event.local,
-            'data_inicio': event.data_inicio,
-            'data_conclusao': event.data_conclusao,
-            'situacao': event.situacao,
-            'status': event.status,
-            'institutions_id': event.institutions_id,
-            'coordenador': event.coordenador,
-        };
 
-        this.http.post('http://sciec.test/org/event/store', this.eventObj).subscribe((res: Response) => {
-            console.log(res);
-            this.fetchData();
-            this.router.navigate(['auth/login']);
-        });
-    };
 
     ngOnInit() {
         this.fetchData();
