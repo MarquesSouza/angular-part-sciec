7e6e4965e0eaf7d480af92230862a9ef05ebec32
Marques <marquesde2013@gmail.com>
2018-08-13 13:29:57 -0300

    Correção de bugs

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -8,11 +8,15 @@ import { HomeComponent } from './home/home.component';
 import {LoginComponent} from './auth/login/login.component';
 import {ValidaCertificadoComponent} from './valida-certificado/valida-certificado.component';
 import {DetalhesEventoComponent} from './home/detalhes-evento/detalhes-evento.component';
+import {MinhasInscricoesComponent} from './participante/minhas-inscricoes/minhas-inscricoes.component';
+import {PerfilComponent} from './participante/perfil/perfil.component';
 
 const routes: Routes =[
     { path: 'home',      component: HomeComponent },
     { path: 'login',           component: LoginComponent },
     { path: 'valida',          component: ValidaCertificadoComponent },
+    { path: 'perfil',          component: PerfilComponent },
+    { path: 'inscricoes',          component: MinhasInscricoesComponent },
     {path: 'detalhes-evento/:id',        component: DetalhesEventoComponent},
       { path: '',          redirectTo: 'home', pathMatch: 'full' },
 
