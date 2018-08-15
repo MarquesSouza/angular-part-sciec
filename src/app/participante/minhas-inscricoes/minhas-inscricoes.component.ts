17c35544ed23bfe9ea91c61138f4e6c02858cffc
Gerverson <gerversonsa@gmail.com>
2018-08-15 16:58:16 -0300

    Geração pdf

--- src/app/participante/minhas-inscricoes/minhas-inscricoes.component.ts
+++ src/app/participante/minhas-inscricoes/minhas-inscricoes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as jsPDF from 'jspdf';
 
 @Component({
   selector: 'app-minhas-inscricoes',
@@ -12,4 +13,11 @@ export class MinhasInscricoesComponent implements OnInit {
   ngOnInit() {
   }
 
+    public download() {
+
+        var doc = jsPDF('l', 'mm', [210, 150]);
+        doc.text(20, 20, 'Hello world!');
+        doc.text(20, 30, 'This is client-side Javascript, pumping out a PDF.');
+        doc.save('Test.pdf');
+    }
 }
